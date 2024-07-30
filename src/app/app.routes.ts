5d1de607bf871eaaeea15a1e7decf4f5e5c86f6d
mahamfathy <mahafathyh@gmail.com>
2024-07-30 16:53:50 +0300

    adding the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProjectComponent } from './projects/project/project.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  { path: 'projects/:id', component: ProjectComponent },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
